df0bbeb9cab090f34311de63ed396f1b988e7e40
M Banks <montensia1@outlook.com>
2019-05-19 01:03:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,8 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.each do |holiday, supplies|
       #binding.pry
       if supplies == "BBQ"
-        put holiday
+        puts holiday
+      end
     end
   end
 end

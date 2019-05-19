55da8828b0e1a06388f32e0e0cc17bd8b9f9def3
M Banks <montensia1@outlook.com>
2019-05-19 00:51:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].each do|x, y|
+  holiday_hash[:winter].collect do|x, y|
     binding.pry
   end
 end

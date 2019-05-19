0c510b83e35fc53cdc23bf5ecf0a4f7ce466db83
M Banks <montensia1@outlook.com>
2019-05-19 01:41:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
-    holidays.each do |x, y|
+    holidays.each do |holiday, supplies|
+    puts holiday.to_s.capitalize + ":"
       binding.pry
     end
   end

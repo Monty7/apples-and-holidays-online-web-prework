9c3f1962b1621e564ae8642f3138deca8685d719
M Banks <montensia1@outlook.com>
2019-05-19 01:25:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    binding.pry
+    puts season.to_s.capitalize + ":"
   end
 end
 
